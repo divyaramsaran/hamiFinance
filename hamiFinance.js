9fc7945c7a17681fea1f8f4980b7ed93a86b9aa6
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-07-05 16:38:07 +0000

    extracts customers

--- hamiFinance.js
+++ hamiFinance.js
@@ -1,5 +1,19 @@
 const extractCustomers = () => {
-  return Deno.readTextFile("./customers.csv").then((data) => {
-    return data.split("\n").map((line) => line.trim());
-  });
+  return Deno.readTextFile("./customers.csv")
+    .then((data) => {
+      return data.split("\n").map((line) => line.trim());
+    })
+    .then((lines) => {
+      const customerWithTransactions = [];
+      lines.forEach((line) => {
+        const customer = line.split(",").map((element) => {
+          return element.trim();
+        });
+
+        customerWithTransactions.push(customer);
+      });
+
+      return customerWithTransactions;
+    });
 };
+extractCustomers();
